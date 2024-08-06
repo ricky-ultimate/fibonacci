0edc411a9a8124e0c646f94610bff02970a33ab8
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-06 12:38:44 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let result = fibonacci(5);
+    println!("{}", result);
+}
+
+fn fibonacci(n: u8) -> u8 {
+    let mut pre: u8 = 0;
+    let mut curr: u8 = 1;
+    let mut count: u8 = 3;
+
+    while count <= n {
+        let temp = curr;
+        curr += pre;
+
+        pre = temp;
+
+        count += 1;
+    }
+    return curr;
 }
